4813d4bbaabc3e3c17545e730b35f1955e2bd478
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-11-21 23:28:16 -0300

    feat: add config module to connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { VideosModule } from './videos/videos.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: ["dist/**/*.entity.js"],
+      migrations: ["dist/shared/database/migrations/*.js"],
+      cli: {
+        "migrationsDir": "src/shared/database/migrations"
+      }
+    }),
     VideosModule
   ],
   controllers: [],
