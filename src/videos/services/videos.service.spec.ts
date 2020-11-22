b7e0a73836c31e91a0db4eefe9fd026efaeb5754
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-11-22 01:22:04 -0300

    test: add video service create test

--- src/videos/services/videos.service.spec.ts
+++ src/videos/services/videos.service.spec.ts
@@ -4,9 +4,12 @@ import { VideosService } from './videos.service';
 
 describe('VideosService', () => {
   let service: VideosService;
+  let repository: VideosRepository;
 
   beforeEach(async () => {
-    const mockRepository = {}
+    const mockRepository = {
+      create: jest.fn()
+    }
 
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -19,9 +22,25 @@ describe('VideosService', () => {
     }).compile();
 
     service = module.get<VideosService>(VideosService);
+    repository = module.get<VideosRepository>(VideosRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('create()', () => {
+    it('should call VideosRepository create with correct values', async () => {
+      const createSpy = jest.spyOn(repository, 'create');
+
+      const mockParam = {
+        title: 'anytitle',
+        url: 'anyurl'
+      }
+
+      await service.create(mockParam);
+
+      expect(createSpy).toHaveBeenCalledWith(mockParam);
+    })
+  })
 });
