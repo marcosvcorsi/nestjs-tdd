5860bba5a0810f90ceeee4027da3573e6f299f56
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-11-22 21:39:49 -0300

    feat: ensure VideoController find all call service

--- src/videos/controllers/videos.controller.ts
+++ src/videos/controllers/videos.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateVideoDto } from '../dtos/create-video.dto';
 import { Video } from '../entities/video.entity';
 import { VideosService } from '../services/videos.service';
@@ -7,6 +7,11 @@ import { VideosService } from '../services/videos.service';
 export class VideosController {
 
   constructor(private readonly videosService: VideosService) {}
+  
+  @Get()
+  async findAll(): Promise<Video[]> {
+    return this.videosService.findAll();
+  }
 
   @Post()
   async create(@Body() createVideoDto: CreateVideoDto): Promise<Video> {
