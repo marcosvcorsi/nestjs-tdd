07ddbc13214d81818ec461850ce5531371eb265d
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-11-25 21:23:37 -0300

    feat: add dtos class validation

--- src/videos/dtos/update-video.dto.ts
+++ src/videos/dtos/update-video.dto.ts
@@ -1,4 +1,12 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class UpdateVideoDto {
+  
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
   url: string;
 }
