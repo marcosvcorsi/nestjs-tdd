0c7200d3c554db62adeb6b6fb8d8f9e86fd1c1a9
Marcos Corsi <marcosvcorsi@gmail.com>
2020-12-28 22:04:17 -0300

    docs: add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+    .setTitle('Videos')
+    .setDescription('The Videos API')
+    .setVersion('1.0')
+    .addTag('videos')
+    .build();
   
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document);
+    
   await app.listen(3333);
 }
 bootstrap();
