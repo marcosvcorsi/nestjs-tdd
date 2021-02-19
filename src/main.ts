08387c4b188a5db8d271ecaceb5bcc519e75d030
Marcos Corsi <marcosvcorsi@gmail.com>
2021-02-18 21:42:19 -0300

    chore: add mockend test

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
   app.useGlobalPipes(new ValidationPipe());
 
   const options = new DocumentBuilder()
